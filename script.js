cb98222082d1c102576e93486f4e3e2102c2c67a
Last Capricorn <48774366+LastCapricorn@users.noreply.github.com>
2023-03-19 14:51:45 +0100

    Add function 'getComputerChoice'

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 window.onload = () => document.querySelector('#start').addEventListener('click', playGame);
 
+function getComputerChoice() {
+  const computerChoice = Math.floor(Math.random() * 3); // random value 0, 1 or 2
+  let result = 'Rock'; // if computerChoice = 0
+  switch (computerChoice) {
+    case 1:
+      result = 'Paper';
+      break;
+    case 2:
+      result = 'Scissors';
+      break;
+  }
+  return result;
+}
+
 function playGame() {
-  console.log('Game started!');
+  document.querySelector('#start').toggleAttribute('disabled');
+  console.log(getComputerChoice());
 }
